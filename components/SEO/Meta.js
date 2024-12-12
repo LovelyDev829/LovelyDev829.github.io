5b69bb8449968619e128e65237483c65bdf1bc7d
LovelyDev829 <lovelydeveloper829@gmail.com>
2024-12-13 01:37:05 +0900

    update description

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -5,9 +5,9 @@ export default function Meta() {
     return (
         <Head>
            /* Primary Meta Tags */
-            <title>Josue Valdivia Portfolio - Computer Engineering Student</title>
+            <title>Josue Valdivia Portfolio - Software Engineer</title>
             <meta charSet="utf-8" />
-            <meta name="title" content="Josue Valdivia Portfolio - Computer Engineering Student" />
+            <meta name="title" content="Josue Valdivia Portfolio - Software Engineer" />
             <meta name="description"
                 content="Josue Valdivia's (LovelyDev829) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="author" content="Josue Valdivia (LovelyDev829)" />
@@ -22,20 +22,20 @@ export default function Meta() {
             /* Search Engine */
             <meta name="image" content="images/logos/fevicon.png" />
             /* Schema.org for Google */
-            <meta itemProp="name" content="Josue Valdivia Portfolio - Computer Engineering Student" />
+            <meta itemProp="name" content="Josue Valdivia Portfolio - Software Engineer" />
             <meta itemProp="description"
                 content="Josue Valdivia's (LovelyDev829) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta itemProp="image" content="images/logos/fevicon.png" />
             /* Twitter */
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="Josue Valdivia Portfolio - Computer Engineering Student" />
+            <meta name="twitter:title" content="Josue Valdivia Portfolio - Software Engineer" />
             <meta name="twitter:description"
                 content="Josue Valdivia's (LovelyDev829) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="twitter:site" content="LovelyDev829" />
             <meta name="twitter:creator" content="LovelyDev829" />
             <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
             /* Open Graph general (Facebook, Pinterest & Google+) */
-            <meta name="og:title" content="Josue Valdivia Portfolio - Computer Engineering Student" />
+            <meta name="og:title" content="Josue Valdivia Portfolio - Software Engineer" />
             <meta name="og:description"
                 content="Josue Valdivia's (LovelyDev829) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="og:image" content="images/logos/logo_1200.png" />
